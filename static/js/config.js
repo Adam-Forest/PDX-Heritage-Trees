779fb86f5c26c581e321ac96a87ff7f803dbbe39
Adam Newlyn <lord.beaker@gmail.com>
2019-09-25 15:51:38 -0700

    initial commit

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYWRhbS1mb3Jlc3QiLCJhIjoiY2swdHQ2bHB1MGd4YzNucHJvZ3FyaDZ4byJ9.PbDjvFoO1BmT6zalpGQc6g";
---
+++ static/js/logic.js
@@ -0,0 +1,69 @@
+var greenIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+// Adding tile layer to the map
+var streets = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+
+var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  Streets: streets,
+  Light: light,
+  Dark: dark
+};
+
+// Creating map object
+var myMap = L.map("map", {
+  center: [45.5051, -122.6750],
+  zoom: 11,
+  layers: streets
+});
+
+
+var url = "https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a4dd778e_26.geojson"
+// Grab data with d3
+
+var markers = L.markerClusterGroup();
+// Grab the data with d3
+d3.json(url, function (response) {
+  console.log(response);
+  // Create a new marker cluster group
+
+  markers.addLayer(L.geoJson(response, {
+    pointToLayer: function (feature, latlng) {
+              return L.marker(latlng, {icon: greenIcon});
+      },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(`<h1>${feature.properties.COMMON}</h1><h1>${feature.properties.SCIENTIFIC}</h1><h2>${feature.properties.NOTES}</h2>`);
+    }
+  }));
+
+  myMap.locate({setView: true, maxZoom: 16});
+  myMap.addLayer(markers);
+
+  L.control.layers(baseMaps).addTo(myMap);
+});
+
