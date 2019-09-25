080f66cc7ba3091cd52887fd32dc09034d61472b
Adam Newlyn <lord.beaker@gmail.com>
2019-09-25 16:29:04 -0700

    removed cache busting

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 var treeIcon = new L.Icon({
   iconUrl: './static/images/marker-icon-2x-tree.png',
-  shadowUrl: './static/images/marker-shadow.png?ver=2',
+  shadowUrl: './static/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
