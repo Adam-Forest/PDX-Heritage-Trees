68fcf1362b5b70e74a4a621ec90e534c1d8d6f16
Adam Newlyn <lord.beaker@gmail.com>
2019-09-25 16:31:23 -0700

    added pirate map

--- static/js/logic.js
+++ static/js/logic.js
@@ -22,17 +22,17 @@ var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?ac
   accessToken: API_KEY
 });
 
-var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+var pirate = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.dark",
+  id: "mapbox.pirates",
   accessToken: API_KEY
 });
 
 var baseMaps = {
   Streets: streets,
   Light: light,
-  Dark: dark
+  Pirate: pirate
 };
 
 // Creating map object
