739f577e1b2bad4798634af6494cc9a34c3d9544
Adam Forest Newlyn <lord.beaker@gmail.com>
2022-11-17 15:19:46 -0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key
 //const API_KEY = "pk.eyJ1IjoiYWRhbS1mb3Jlc3QiLCJhIjoiY2swdHQ2bHB1MGd4YzNucHJvZ3FyaDZ4byJ9.PbDjvFoO1BmT6zalpGQc6g";
-const API_KEY = "pk.eyJ1IjoiYWRhbWZvcmVzdCIsImEiOiJjbGFsbnZrN2QwMjlxNDBudnphZmo5cXF4In0.1wbAYOiqng3xeHLA6z_Vjw";
+const API_KEY = "pk.eyJ1IjoiYWRhbWZvcmVzdCIsImEiOiJja3c1aXh1MzUwcjFxMm9vOHo0Ym03cHE3In0.JxTerOnAuTnHFL66BbybhA";
