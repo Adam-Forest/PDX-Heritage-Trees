4f22277fa05bbaff4da47fe605eb6f9d094b5066
Adam Forest Newlyn <lord.beaker@gmail.com>
2022-11-18 08:52:09 -0800

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,6 +13,7 @@ var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   tileSize: 512,
+  zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 });
